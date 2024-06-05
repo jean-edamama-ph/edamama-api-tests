18a23063c234ba15fe650d90f75786691d985710
abernal <antoinette@edamama.ph>
2024-06-05 13:32:14 +0800

    test data SC

--- libraries/data/testData.py
+++ libraries/data/testData.py
@@ -359,3 +359,33 @@ class tss:
                 "prodId": "62e0b9485c52803d08827792",
                 "variantId": "62e0cf2e38d84df35cc1a1a2"
         }
+        scTssScProductM= {
+                "listName": "tss-sc-product-m-1716173524214",
+                "prodId": "664abad46e08f1e7ab859c71",
+                "variantId": "664abae06e08f1e7ab859c92"
+        }
+        scTssScProductN= {
+                "listName": "tss-sc-product-n-1716173612380",
+                "prodId": "664abb2c1cf02bf66729fa12",
+                "variantId": "664abb3c1cf02bf66729fa32"
+        }
+        scTssScProductO= {
+                "listName": "tss-sc-product-o-1716173683506",
+                "prodId": "664abb738206beb7c2c131a1",
+                "variantId": "664abb7fdc547b40c17e632b"
+        }
+        scTssScProductP= {
+                "listName": "tss-sc-product-p-1716173751898",
+                "prodId": "664abbb7dc547b40c17e6361",
+                "variantId": "664abbc486fce3d2021270e8"
+        }
+        scTssScProductQ= {
+                "listName": "tss-sc-product-q-1716173809492",
+                "prodId": "664abbf1dc547b40c17e637e",
+                "variantId": "664abbff6e08f1e7ab859d15"
+        }
+        scTssScProductR= {
+                "listName": "tss-sc-product-r-1716173876592",
+                "prodId": "664abc3486fce3d2021271cd",
+                "variantId": "664abc478206beb7c2c1321d"
+        }
